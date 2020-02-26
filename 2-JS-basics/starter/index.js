b4dc94a687f10a3c0ce51e5d5f7f92c21fdadec4
stayacid <stayacid@gmail.com>
2020-02-26 13:11:19 +0300

    end 4th challenge

--- 2-JS-basics/starter/index.js
+++ 2-JS-basics/starter/index.js
@@ -94,6 +94,7 @@ const bills = [124, 48, 268]
 
 const calc = (bills) => {
   let tips = []
+  //count tips
   bills.forEach(bill => {
     switch (true) {
       case bill < 50:
@@ -107,7 +108,63 @@ const calc = (bills) => {
         break;
     }
   });
-  console.log(tips);
+
+  console.log(`Tips: ${tips}`);
+  //sum 2 arrays
+  const finalBills = bills.map((bill, index) => {
+    return bill + tips[index]
+  });
+  console.log(`Final Bills: ${finalBills}`);
 }
 
-calc(bills)
+//calc(bills)
+
+/*****************************
+* CODING CHALLENGE 4
+*/
+
+/*
+Let's remember the first coding challenge where Mark and John compared their BMIs. Let's now implement the same functionality with objects and methods.
+1. For each of them, create an object with properties for their full name, mass, and height
+2. Then, add a method to each object to calculate the BMI. Save the BMI to the object and also return it from the method.
+3. In the end, log to the console who has the highest BMI, together with the full name and the respective BMI. Don't forget they might have the same BMI.
+
+Remember: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+GOOD LUCK 😀
+*/
+const calcBmi = (weight, height) => { 
+  return Math.round(weight / Math.pow(height, 2));
+};
+
+const mark = {
+  fullName: "Markimillian",
+  mass: 62,
+  height: 1.72,
+  getBmi() {
+    this.bmi = calcBmi(this.mass, this.height)
+  }
+}
+mark.getBmi()
+
+const john = {
+  fullName: "Johnas",
+  mass: 75,
+  height: 1.8,
+  getBmi() {
+    this.bmi = calcBmi(this.mass, this.height)
+  }
+}
+john.getBmi()
+
+switch (true) {
+  case mark.bmi > john.bmi:
+    console.log(`${mark.fullName} has bigger BMI: ${mark.bmi}`);
+    break;
+  case john.bmi > mark.bmi:
+    console.log(`${john.fullName} has bigger BMI: ${john.bmi}`);
+    break;
+  default:
+    console.log(`Guys have equal BMI: ${mark.bmi}`);
+    break;
+}
