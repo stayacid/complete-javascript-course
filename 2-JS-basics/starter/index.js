588a34175d5ab1d73929cea0680fb8a57e01edea
stayacid <stayacid@gmail.com>
2020-02-27 10:52:56 +0300

    doing 5th challenge

--- 2-JS-basics/starter/index.js
+++ 2-JS-basics/starter/index.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /** ***************************
 * CODING CHALLENGE 1
@@ -139,7 +139,7 @@ const calcBmi = (weight, height) => {
 
 const mark = {
   fullName: "Markimillian",
-  mass: 62,
+  mass: 79,
   height: 1.72,
   getBmi() {
     this.bmi = calcBmi(this.mass, this.height)
@@ -157,7 +157,7 @@ const john = {
 }
 john.getBmi()
 
-switch (true) {
+/*switch (true) {
   case mark.bmi > john.bmi:
     console.log(`${mark.fullName} has bigger BMI: ${mark.bmi}`);
     break;
@@ -167,4 +167,106 @@ switch (true) {
   default:
     console.log(`Guys have equal BMI: ${mark.bmi}`);
     break;
+}*/
+
+
+/*****************************
+* CODING CHALLENGE 5
+*/
+
+/*
+Remember the tip calculator challenge? Let's create a more advanced version using everything we learned!
+
+This time, John and his family went to 5 different restaurants. The bills were $124, $48, $268, $180 and $42.
+John likes to tip 20% of the bill when the bill is less than $50, 15% when the bill is between $50 and $200, and 10% if the bill is more than $200.
+
+Implement a tip calculator using objects and loops:
+1. Create an object with an array for the bill values
+2. Add a method to calculate the tip
+3. This method should include a loop to iterate over all the paid bills and do the tip calculations
+4. As an output, create 1) a new array containing all tips, and 2) an array containing final paid amounts (bill + tip). HINT: Start with two empty arrays [] as properties and then fill them up in the loop.
+
+
+EXTRA AFTER FINISHING: Mark's family also went on a holiday, going to 4 different restaurants. The bills were $77, $375, $110, and $45.
+Mark likes to tip 20% of the bill when the bill is less than $100, 10% when the bill is between $100 and $300, and 25% if the bill is more than $300 (different than John).
+
+5. Implement the same functionality as before, this time using Mark's tipping rules
+6. Create a function (not a method) to calculate the average of a given array of tips. HINT: Loop over the array, and in each iteration store the current sum in a variable (starting from 0). After you have the sum of the array, divide it by the number of elements in it (that's how you calculate the average)
+7. Calculate the average tip for each family
+8. Log to the console which family paid the highest tips on average
+
+GOOD LUCK 😀
+*/
+
+class Bills {
+  constructor(bills) {
+    this.bills = bills;
+    this.tips = [];
+    this.finalBills = [];
+  }
+
+  calcTip() {
+    for (let i = 0; i < this.bills.length; i++) {
+
+      const el = this.bills[i];
+      let tipSize = 0;
+      //calc tip size
+      switch (true) {
+        case el < 50:
+          tipSize = 0.2
+          break;
+        case el >= 50 && el < 200:
+          tipSize = 0.15
+          break;
+        default:
+          tipSize = 0.1
+          break;
+      }
+      //make tips array
+      const tip = Math.round(this.bills[i] * tipSize)
+      this.tips.push(tip);
+
+      //make final array
+      this.finalBills.push(this.bills[i] + tip)
+    }
+  }
+
 }
+
+let johnb = new Bills([124, 48, 268, 180, 42]);
+johnb.calcTip()
+console.log(johnb);
+
+const johnBills = {
+  bills: [124, 48, 268, 180, 42],
+  tips: [],
+  finalBills: [],
+  calcTip() {
+    for (let i = 0; i < this.bills.length; i++) {
+
+      const el = this.bills[i];
+      let tipSize = 0;
+      //calc tip size
+      switch (true) {
+        case el < 50:
+          tipSize = 0.2
+          break;
+        case el >= 50 && el < 200:
+          tipSize = 0.15
+          break;
+        default:
+          tipSize = 0.1
+          break;
+      }
+      //make tips array
+      const tip = Math.round(this.bills[i] * tipSize)
+      this.tips.push(tip);
+
+      //make final array
+      this.finalBills.push(this.bills[i] + tip)
+    }
+  }
+}
+
+//johnBills.calcTip()
+
