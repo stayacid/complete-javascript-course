b9ad6fc6802a6ff18b0e4521528d93b081848713
stayacid <stayacid@gmail.com>
2020-03-04 11:19:06 +0300

    add second dice. Need to fix

--- 4-DOM-pig-game/starter/app.js
+++ 4-DOM-pig-game/starter/app.js
@@ -11,23 +11,32 @@ GAME RULES:
 let scores;
 let roundScore;
 let activePlayer;
-let dice = 0;
-let prevDice = 0;
+let dices = [0, 0];
+let prevDices = [0, 0];
 let gamePlaying;
+let maxScore = 100;
 
-const diceImg = document.querySelector('.dice');
+// set max score
+const maxScoreInput = document.querySelector('.maxScore input');
+maxScoreInput.value = maxScore;
+maxScoreInput.addEventListener('change', () => {
+  maxScore = +maxScoreInput.value; // '+' is here because type='number' still return a string
+});
+
 
 function init() {
+  dices = [0, 0];
   scores = [0, 0];
+  prevDices = [0, 0];
   roundScore = 0;
   activePlayer = 0;
   gamePlaying = true;
 
   // hide dice before first move
-  diceImg.style.display = 'none';
-
+  //dice0.style.display = 'none';
   // zero all
   for (let i = 0; i < 2; i++) {
+    document.getElementById(`dice-${i}`).style.display = 'none';
     document.getElementById(`score-${i}`).textContent = '0';
     document.getElementById(`current-${i}`).textContent = '0';
     document.getElementById(`name-${i}`).textContent = `Player ${i + 1}`;
@@ -56,25 +65,29 @@ function nextPlayer() {
 // dice actions
 document.querySelector('.btn-roll').addEventListener('click', () => {
   if (gamePlaying) {
-    dice !== 0 ? prevDice = dice : false;
-    console.log(scores);
-    // 1. get random number
-    dice = Math.floor(Math.random() * 6) + 1; // because math.ceil and math.round can return 0
-    // 2.display the result
-    diceImg.style.display = 'block';
-    diceImg.src = `dice-${dice}.png`;
-    diceImg.classList.toggle('roll');
-    // 3. update the round score if the rolled number was not a 1
-    if (dice !== 1) {
-      roundScore += dice;
-      document.getElementById(`current-${activePlayer}`).textContent = roundScore;
-      if (dice === 6 && prevDice === 6) {
-        scores[activePlayer] = 0;
-        document.getElementById(`score-${activePlayer}`).textContent = '0';
+    for (let i = 0; i < dices.length; i++) {
+      let dice = dices[i];
+      // save value to prevDise from 2nd move
+      dice !== 0 ? prevDice[i] = dice : false;
+
+      // 1. get random number
+      dice = Math.floor(Math.random() * 6) + 1; // because math.ceil and math.round can return 0
+      // 2.display the result
+      document.getElementById(`dice-${i}`).style.display = 'block';
+      document.getElementById(`dice-${i}`).src = `dice-${dice}.png`;
+      document.getElementById(`dice-${i}`).classList.toggle('roll');
+      // 3. update the round score if the rolled number was not a 1
+      if (dice !== 1) {
+        roundScore += dice;
+        document.getElementById(`current-${activePlayer}`).textContent = roundScore;
+        /* if (dice === 6 && prevDice === 6) {
+          scores[activePlayer] = 0;
+          document.getElementById(`score-${activePlayer}`).textContent = '0';
+          nextPlayer();
+        } */
+      } else {
         nextPlayer();
       }
-    } else {
-      nextPlayer();
     }
   }
 });
@@ -87,9 +100,12 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
     // Update the UI
     document.querySelector(`#score-${activePlayer}`).textContent = scores[activePlayer];
     // Check if player won the game
-    if (scores[activePlayer] >= 20) {
+    if (scores[activePlayer] >= maxScore) {
       document.getElementById(`name-${activePlayer}`).textContent = 'Winner!';
-      diceImg.style.display = 'none';
+      // hide dices
+      for (let i = 0; i < 2; i++) {
+        document.getElementById(`dice-${i}`).style.display = 'none';
+      }
       document.querySelector(`.player-${activePlayer}-panel`).classList.add('winner');
       document.querySelector(`.player-${activePlayer}-panel`).classList.remove('active');
       gamePlaying = false;
@@ -98,3 +114,6 @@ document.querySelector('.btn-hold').addEventListener('click', () => {
     }
   }
 });
+
+// fix - if first dice == 1 then second dice score will move to next player
+// add - if both dices == 6, round will be passed to another player
