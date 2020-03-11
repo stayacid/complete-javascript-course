11c900c399c4cad14ab9b35661a695a10add8ff9
stayacid <stayacid@gmail.com>
2020-03-11 11:03:01 +0300

    doing quiz

--- 5-advanced-JS/starter/script.js
+++ 5-advanced-JS/starter/script.js
@@ -58,6 +58,11 @@ c) correct answer (I would use a number for this)
 7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
 */
 
+const questionNode = document.querySelector('h2');
+const answerNode = document.querySelector('ol');
+const formNode = document.querySelector('form');
+const scoreNode = document.querySelector('p span');
+
 const Question = function (question, answers, correntAnswer) {
   this.question = question;
   this.answers = answers;
@@ -76,9 +81,35 @@ const thirdQuestion = new Question('Which year is it now ?', [2019, 2020, 2021],
 
 const questions = [firstQuestion, secondQuestion, thirdQuestion];
 
-function askQuestion(params) {
-  const currentQuestion = Math.floor(Math.random() * questions.length);
+let currentQuestion;
+
+function askQuestion() {
+  // get random answer
+  const currentQuestionNumber = Math.floor(Math.random() * questions.length);
+  // save this question
+  currentQuestion = questions.slice(currentQuestionNumber, currentQuestionNumber + 1)[0];
+  console.log(currentQuestion);
+  // cut question from array
+  questions.splice(currentQuestionNumber, 1);
+  // put text in question
+  questionNode.textContent = currentQuestion.question;
+  // delete previous list
+  while (answerNode.firstChild) {
+    answerNode.removeChild(answerNode.firstChild);
+  }
+  // create new list
+  const fragment = new DocumentFragment();
+  for (let i = 0; i < currentQuestion.answers.length; i++) {
+    const li = document.createElement('li');
+    li.textContent = currentQuestion.answers[i];
+    fragment.append(li);
+  }
+  answerNode.append(fragment);
 }
+
+askQuestion();
+
+
 
 /* 
   1. Берем случайный вопрос (сделано)
