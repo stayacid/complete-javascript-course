f493992091118357d41f3213d5f73b3abb822f13
stayacid <stayacid@gmail.com>
2020-03-16 12:53:45 +0300

    wip

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,23 +1,84 @@
 const budgetController = (function () {
+  const Expense = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
+  const Income = (id, description, value) => {
+    this.id = id;
+    this.description = description;
+    this.value = value;
+  };
 
+  const data = {
+    allItems: {
+      exp: [],
+      inc: [],
+    },
+    totals: {
+      exp: 0,
+      inc: 0,
+    },
+  };
+
+  return {
+    addItem(type, des, val) {
+      let newItem;
+      let ID = 0;
+
+      if (type === 'exp') {
+        newItem = new Expense(ID, des, val);
+      } else if (type === 'inc') {
+        newItem = new Income(ID, des, val);
+      }
+      data.allItems[type].push(newItem);
+      return newItem;
+    },
+  };
 }());
 
 const UIController = (function () {
+  const DOMstrings = {
+    inputType: '.add__type',
+    inputdescription: '.add__description',
+    inputValue: '.add__value',
+    inputBtn: '.add__btn',
+  };
+
   return {
+    getDOMstrings() {
+      return DOMstrings;
+    },
     getInput() {
-      // const type = document.querySelector();
+      return {
+        type: document.querySelector(DOMstrings.inputType).value, // will be inc or exp
+        description: document.querySelector(DOMstrings.inputdescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
     },
   };
 }());
 
 const controller = (function (budgetCtrl, UICtrl) {
-  const ctrlAddItem = () => {
-    console.log('puk');
+  const setupIventListeners = () => {
+    const DOM = UICtrl.getDOMstrings();
+    document.querySelector(DOM.inputBtn).addEventListener('click', ctrlAddItem);
+    document.addEventListener('keyup', (e) => {
+      e.key === "Enter" ? ctrlAddItem() : false;
+    });
   };
 
-  document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+  const ctrlAddItem = () => {
+    const input = UICtrl.getInput();
+    console.log(input);
+  };
 
-  document.addEventListener('keyup', (e) => {
-    e.key === "Enter" ? ctrlAddItem() : false;
-  });
+  return {
+    init() {
+      console.log('test');
+      setupIventListeners();
+    },
+  };
 }(budgetController, UIController));
+
+controller.init();
