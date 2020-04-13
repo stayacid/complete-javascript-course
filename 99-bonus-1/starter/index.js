e8e6525370353acbaede5997efc2424ce43fa687
stayacid <stayacid@gmail.com>
2020-04-13 09:51:56 +0300

    doing node

--- 99-bonus-1/starter/index.js
+++ 99-bonus-1/starter/index.js
@@ -1,12 +1,40 @@
 const fs = require('fs');
 const http = require('http');
+const url = require('url');
 
 const json = fs.readFileSync(`${__dirname}/data/data.json`, 'utf-8');
 const laptopData = JSON.parse(json);
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-type': 'text/html' });
-  res.end('This is the response!');
+  const pathName = url.parse(req.url, true).pathname;
+  const { id } = url.parse(req.url, true).query;
+
+  if (pathName === '/products' || pathName === '') {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    res.end('This is the PRODUCT page');
+  }
+
+  else if (pathName === '/laptop' && id < laptopData.length) {
+    res.writeHead(200, { 'Content-type': 'text/html' });
+    fs.readFile(`${__dirname}/templates/template-laptop.html`, 'utf-8', (err, data) => {
+      const laptop = laptopData[id];
+      let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+      output = output.replace(/{%IMAGE%}/g, laptop.image);
+      output = output.replace(/{%PRICE%}/g, laptop.price);
+      output = output.replace(/{%SCREEN%}/g, laptop.screen);
+      output = output.replace(/{%CPU%}/g, laptop.cpu);
+      output = output.replace(/{%STORAGE%}/g, laptop.storage);
+      output = output.replace(/{%RAM%}/g, laptop.ram);
+      output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+      res.end(output);
+    });
+  }
+
+  else {
+    res.writeHead(404, { 'Content-type': 'text/html' });
+    res.end('URL was not found on the server ');
+  }
+
 });
 
 server.listen(1337, '127.0.0.1', () => {
